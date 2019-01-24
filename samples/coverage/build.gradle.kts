878cb5133ed736b7d1f585ff5886ae3c632a6ac8
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2019-01-24 11:32:52 +0700

    Added `collectGcov` task

--- samples/coverage/build.gradle.kts
+++ samples/coverage/build.gradle.kts
@@ -27,3 +27,26 @@ kotlin {
         compilations["main"].extraOpts = mutableListOf("-Xgcov-dir=$buildDir/gcov")
     }
 }
+
+// Soon will be implemented as part of Gradle plugin.
+tasks.create("collectGcov") {
+    dependsOn("macosTest")
+
+    description = "Create .gcov files based on .gdca and .gcno"
+
+    fileTree("$buildDir/gcov").matching { include("**/.gcda") }.forEach { file ->
+        exec {
+            // TODO: change workingDir to something more useful
+            workingDir = file.parentFile
+            // TODO: use `llvm-cov` from distribution
+            commandLine("llvm-cov", "gcov", file.name)
+        }
+    }
+
+}
+
+tasks.create("createCoverageReport") {
+    dependsOn("collectGcov")
+
+    description = "Create lcov report"
+}
