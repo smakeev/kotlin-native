f5588cc76fc10a0bbfb64cb36e97696cbb0cf6f8
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2019-02-13 11:56:01 +0700

    wip

--- llvmCoverageMappingC/src/main/cpp/CoverageMappingC.cpp
+++ llvmCoverageMappingC/src/main/cpp/CoverageMappingC.cpp
@@ -79,6 +79,89 @@ LLVMAddFunctionMappingRecord(LLVMContextRef context, const char *name, uint64_t 
     return llvm::wrap(addFunctionMappingRecord(*llvm::unwrap(context), name, hash, coverageMapping));
 }
 
+static void emit() {
+    if (FunctionRecords.empty())
+        return;
+    llvm::LLVMContext &Ctx = CGM.getLLVMContext();
+    auto *Int32Ty = llvm::Type::getInt32Ty(Ctx);
+
+    // Create the filenames and merge them with coverage mappings
+    llvm::SmallVector<std::string, 16> FilenameStrs;
+    llvm::SmallVector<StringRef, 16> FilenameRefs;
+    FilenameStrs.resize(FileEntries.size());
+    FilenameRefs.resize(FileEntries.size());
+    for (const auto &Entry : FileEntries) {
+        auto I = Entry.second;
+        FilenameStrs[I] = normalizeFilename(Entry.first->getName());
+        FilenameRefs[I] = FilenameStrs[I];
+    }
+
+    std::string FilenamesAndCoverageMappings;
+    llvm::raw_string_ostream OS(FilenamesAndCoverageMappings);
+    CoverageFilenamesSectionWriter(FilenameRefs).write(OS);
+    std::string RawCoverageMappings =
+            llvm::join(CoverageMappings.begin(), CoverageMappings.end(), "");
+    OS << RawCoverageMappings;
+    size_t CoverageMappingSize = RawCoverageMappings.size();
+    size_t FilenamesSize = OS.str().size() - CoverageMappingSize;
+    // Append extra zeroes if necessary to ensure that the size of the filenames
+    // and coverage mappings is a multiple of 8.
+    if (size_t Rem = OS.str().size() % 8) {
+        CoverageMappingSize += 8 - Rem;
+        for (size_t I = 0, S = 8 - Rem; I < S; ++I)
+            OS << '\0';
+    }
+    auto *FilenamesAndMappingsVal =
+            llvm::ConstantDataArray::getString(Ctx, OS.str(), false);
+
+    // Create the deferred function records array
+    auto RecordsTy =
+            llvm::ArrayType::get(FunctionRecordTy, FunctionRecords.size());
+    auto RecordsVal = llvm::ConstantArray::get(RecordsTy, FunctionRecords);
+
+    llvm::Type *CovDataHeaderTypes[] = {
+#define COVMAP_HEADER(Type, LLVMType, Name, Init) LLVMType,
+#include "llvm/ProfileData/InstrProfData.inc"
+    };
+    auto CovDataHeaderTy =
+            llvm::StructType::get(Ctx, makeArrayRef(CovDataHeaderTypes));
+    llvm::Constant *CovDataHeaderVals[] = {
+#define COVMAP_HEADER(Type, LLVMType, Name, Init) Init,
+#include "llvm/ProfileData/InstrProfData.inc"
+    };
+    auto CovDataHeaderVal = llvm::ConstantStruct::get(
+            CovDataHeaderTy, makeArrayRef(CovDataHeaderVals));
+
+    // Create the coverage data record
+    llvm::Type *CovDataTypes[] = {CovDataHeaderTy, RecordsTy,
+                                  FilenamesAndMappingsVal->getType()};
+    auto CovDataTy = llvm::StructType::get(Ctx, makeArrayRef(CovDataTypes));
+    llvm::Constant *TUDataVals[] = {CovDataHeaderVal, RecordsVal,
+                                    FilenamesAndMappingsVal};
+    auto CovDataVal =
+            llvm::ConstantStruct::get(CovDataTy, makeArrayRef(TUDataVals));
+    auto CovData = new llvm::GlobalVariable(
+            CGM.getModule(), CovDataTy, true, llvm::GlobalValue::InternalLinkage,
+            CovDataVal, llvm::getCoverageMappingVarName());
+
+    CovData->setSection(getCoverageSection(CGM));
+    CovData->setAlignment(8);
+
+    // Make sure the data doesn't get deleted.
+    CGM.addUsedGlobal(CovData);
+    // Create the deferred function records array
+    if (!FunctionNames.empty()) {
+        auto NamesArrTy = llvm::ArrayType::get(llvm::Type::getInt8PtrTy(Ctx),
+                                               FunctionNames.size());
+        auto NamesArrVal = llvm::ConstantArray::get(NamesArrTy, FunctionNames);
+        // This variable will *NOT* be emitted to the object file. It is used
+        // to pass the list of names referenced to codegen.
+        new llvm::GlobalVariable(CGM.getModule(), NamesArrTy, true,
+                                 llvm::GlobalValue::InternalLinkage, NamesArrVal,
+                                 llvm::getCoverageUnusedNamesVarName());
+    }
+}
+
 
 
 
